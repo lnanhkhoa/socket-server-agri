9e888fbc95713eefd59d295617ead004d510ba0c
lnanhkhoa <lnanhkhoa303@gmail.com>
2019-04-04 10:59:30 +0700

    produc

---
+++ config/config.production.js
@@ -0,0 +1,52 @@
+const util = require('../core/util');
+
+let config = {
+    configFile: 'development',
+    isDevelopment: true,
+    cluster: {
+        workerCount: 1,
+    },
+    apiServer: {
+        port: 1234,
+        path: '/api',
+        localSecretKey: undefined,
+    },
+    lalamove: {
+        host: undefined,
+        country: undefined,
+        customerId: undefined,
+        key: undefined,
+        secret: undefined,
+    },
+    redmine: {
+        host: undefined,
+        apiKey: undefined,
+        requestTimeout: 1000,
+    },
+    session: {
+        adminCheckUrl: undefined,
+        merchantCheckUrl: undefined,
+        userCheckUrl: undefined,
+    },
+    redis: {
+        host: undefined,
+        port: undefined,
+        prefix: undefined,
+    },
+    database: {
+        agrismart: {
+            client: 'mysql',
+            connection: {
+                host: '127.0.0.1',
+                port: 3306,
+                user: 'agri',
+                password: 'agri@123',
+                database: 'agrismart',
+                timezone: 'UTC',
+                dateStrings: true,
+            },
+        },
+    },
+};
+
+module.exports = config;
